9fec2b18769f9b305fe6a077adebc80c0dc135bc
Leo Yang <leo19941227@gmail.com>
2020-08-01 18:54:04 +0800

    chagne onlinepreprocessor import location

--- run_downstream.py
+++ run_downstream.py
@@ -10,7 +10,7 @@ import numpy as np
 from shutil import copyfile
 from torch.utils.data import DataLoader
 from torch.nn.utils.rnn import pad_sequence
-from dataloader import OnlinePreprocessor
+from utility.preprocessor import OnlinePreprocessor
 from transformer.nn_transformer import TRANSFORMER
 from downstream.model import dummy_upstream
 from runner import Runner
