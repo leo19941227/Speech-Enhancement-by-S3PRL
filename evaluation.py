37f0d38a10aea1ecf676eb0ceedb243af2c8c683
Leo Yang <leo19941227@gmail.com>
2020-08-08 04:07:27 +0800

    warning when wav values too small instead of traceback

--- evaluation.py
+++ evaluation.py
@@ -4,13 +4,15 @@ from pystoi import stoi
 
 def pesq_nb_eval(src, tar, sr=16000):
     assert src.ndim == 1 and tar.ndim == 1
-    assert not np.allclose(src.sum(), 0.0, atol=1e-6) and not np.allclose(tar.sum(), 0.0, atol=1e-6)
+    if np.allclose(src.sum(), 0.0, atol=1e-10) or np.allclose(tar.sum(), 0.0, atol=1e-10):
+        print(f'[Evaluation] wav values too small: src {src.sum()}, tar {tar.sum()}')
     mos_lqo = pesq(sr, tar, src, 'nb')
     return mos_lqo
 
 def pesq_wb_eval(src, tar, sr=16000):
     assert src.ndim == 1 and tar.ndim == 1
-    assert not np.allclose(src.sum(), 0.0, atol=1e-6) and not np.allclose(tar.sum(), 0.0, atol=1e-6)
+    if np.allclose(src.sum(), 0.0, atol=1e-10) or np.allclose(tar.sum(), 0.0, atol=1e-10):
+        print(f'[Evaluation] wav values too small: src {src.sum()}, tar {tar.sum()}')
     mos_lqo = pesq(sr, tar, src, 'wb')
     return mos_lqo
 
