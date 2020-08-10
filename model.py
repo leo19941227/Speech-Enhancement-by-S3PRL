0d6ba1752212fb91f3b2f59c9d807955258e2659
Leo Yang <leo19941227@gmail.com>
2020-08-10 22:58:29 +0800

    update residual model

--- model.py
+++ model.py
@@ -38,7 +38,8 @@ class LSTM(nn.Module):
 
 
 class Residual(nn.Module):
-    def __init__(self, input_size=201, output_size=201, hidden_size=201, num_layers=3, bidirectional=False, **kwargs):
+    def __init__(self, input_size=201, output_size=201, hidden_size=201, num_layers=3, bidirectional=False,
+                 cmvn=False, eps=1e-6, **kwargs):
         super(Residual, self).__init__()
         self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size,
                             num_layers=num_layers, batch_first=True, bidirectional=bidirectional)
@@ -46,6 +47,8 @@ class Residual(nn.Module):
             nn.Linear(hidden_size, output_size), nn.ReLU())
         self.init_weights()
         self.bidirectional = bidirectional
+        self.cmvn = cmvn
+        self.eps = eps
 
     def init_weights(self):
         for name, param in self.named_parameters():
@@ -58,6 +61,8 @@ class Residual(nn.Module):
 
     def forward(self, features, linears, **kwargs):
         offset, _ = self.lstm(features)
+        if self.cmvn:
+            offset = (offset - offset.mean(dim=1, keepdim=True)) / (offset.std(dim=1, keepdim=True) + self.eps)
         offset = self.scaling_layer(offset)
         predicted = linears * offset
         return predicted
