3f6593a2c965f8ca14c0da89b53bbeffaebac0d7
Leo Yang <leo19941227@gmail.com>
2020-10-31 19:22:38 +0800

    add run scripts

---
+++ run_uniform.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+noise_dir="../datasets/noise_data/Nonspeech1/"
+
+for noise_type in $(ls $noise_dir);
+do
+    noise_str=$noise_dir"/"$noise_type
+    python3 run_downstream.py --name uniform_p232_$noise_type --expdir result/active/all_noise --ckpt ../S3PRL/result/se7-noise2clean/states-500000.ckpt --ckpt2 ../S3PRL/result/noisy2noise-6layer-3072/states-500000.ckpt --downstream LSTM --dckpt result/active/pretrain/3lstm-l1-v2/states-500000.ckpt --from_rawfeature --config config/active.yaml --test_speech "../datasets/speech_data/clean_testset_wav_16k/p232*" --test_noise $noise_str
+done
+
